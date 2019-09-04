7ef2ed7ba3465f58767da6951dd93758bbc48f6a
hzbhzb <1012734765@qq.com>
2019-09-04 16:12:05 +0800

    35. 搜索插入位置解法

--- src/leetcode/SearchAndInsert.java
+++ src/leetcode/SearchAndInsert.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 /**
+ * 35. 搜索插入位置
  * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 You may assume no duplicates in the array.
