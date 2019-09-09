0a844f2cf3b23905166dcc0db913c94c52cf6858
hzbhzb <1012734765@qq.com>
2019-09-09 09:25:00 +0800

    增加两种解法

--- src/leetcode/StrStr.java
+++ src/leetcode/StrStr.java
@@ -1,11 +1,23 @@
 package leetcode;
+/**
+ * 28. 实现 strStr()
+ * 实现 strStr() 函数。
 
+ *给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+ * @author huangzhebin
+ *
+ */
 public class StrStr {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        strStr("hello", "ll");
-	}
+
+	
+	/**
+	 * 解法一
+	 * @param haystack
+	 * @param needle
+	 * @return
+	 */
 
 	public static int strStr(String haystack, String needle) {
 
@@ -26,6 +38,25 @@ public class StrStr {
 			}
 		}
         return -1;
+	}
+	
+	/**
+	 * 解法二
+	 * @param haystack
+	 * @param needle
+	 * @return
+	 */
+	public static int strStr2(String haystack, String needle) {
+
+		 if (needle == null || needle.length() == 0)
+	            return 0;
+	        if (needle.length() > haystack.length())
+	        	return -1;
+	        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+			  if (haystack.substring(i, i + needle.length()).equals(needle))
+				  return i;
+			}
+	        return -1;
 	}
 
 }
